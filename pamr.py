b1a32ad3211d62337f0ab9c950c8289538367861
EthanBraun <braune@oregonstate.edu>
2018-06-29 13:21:16 -0700

    Removed unnecessary trace statements and NaN checks

--- pamr.py
+++ pamr.py
@@ -36,32 +36,15 @@ class Portfolio():
 		prevValues = np.multiply(prevWeights, prevValue)
 		currentValues = np.multiply(rateRatios, prevValues)
 		currentVal = sum(currentValues)
-		#print('Current portfolio value with new rates: ' + str(currentVal))
 		
-		for i in range(len(newWeights)):
-			if isnan(newWeights[i]):
-				print('\n\n\n\t\tnewWeights is nan at: ' + str(i) + '\n\n\n')
-
-		for i in range(len(self.weights)):
-			if isnan(self.weights[i]):
-				print('\n\n\n\t\tself.weights is nan at: ' + str(i) + '\n\n\n')
-
 		# Calculate difference between current and new weights
 		weightDelta = np.subtract(newWeights, self.weights)
 
-		for i in range(len(weightDelta)):
-			if isnan(weightDelta[i]):
-				print('\n\n\n\t\tweightDelta is nan at: ' + str(i) + '\n\n\n')
-
-		if isnan(self.value):
-			print('\n\n\n\t\tself.value is nan\n\n\n')
 		valueDelta = [(self.value * delta) for delta in weightDelta]
 
 		# Calculate BTC being bought
 		buy = self.tradePer * -sum([v if (v < 0) else 0 for v in valueDelta])
-		#print('\nBTC bought: ' + str(buy))
 
-		#print('\t\t\tMax value delta: ' + str(max(valueDelta)))
 		posValDeltas = {}
 		for i in range(len(valueDelta)):
 			if valueDelta[i] > 0:
@@ -84,12 +67,9 @@ class Portfolio():
 		# Calculate new value
 		newValues = np.add(currentValues, realValDeltas)
 		newValue = sum(newValues)
-		#print('Portfolio value after shift: ' + str(newValue))
 		self.setValue(newValue)
 	
 		self.setWeights(np.divide(newValues, newValue))
-		#print('\nNew weights:')
-		#print(str(list(self.getWeights())) + '\n')
 
 	# Simulate the pamr agent over a set of data and return the final portfolio value
 	def simulate(self, fData):	
@@ -103,18 +83,10 @@ class Portfolio():
 			for j in range(len(symbols)):
 				lastClose = fData[i+1][j]
 				prevClose = fData[i][j]
-				if isnan(lastClose):
-					print('\n\n\n\t\tlast close is nan at (' + str(i) + ', ' + str(j) + ')\n\n\n')
-				if isnan(prevClose):
-					print('\n\n\n\t\tprev close is nan at (' + str(i) + ', ' + str(j) + ')\n\n\n')
 				curRates.append(lastClose)
 				prevRates.append(prevClose)
-				if isnan(lastClose / prevClose):
-					print('\n\n\n\t\tx is nan at (' + str(i) + ', ' + str(j) + ')\n\n\n')
 				x.append(lastClose / prevClose)
 				values.append(self.getWeights()[j] * self.getValue() * x[j])
-
-
 	
 			prevValue = self.getValue()
 			self.setValue(sum(values))
@@ -123,41 +95,22 @@ class Portfolio():
 			prevWeights = self.getWeights()
 			
 			if not self.noop:
-				for j in range(len(b)):
-					if isnan(b[j]):
-						print('\n\n\n\t\tb is nan at: (' + str(i) + ', ' + str(j) + ')\n\n\n')
 				self.setWeights(b)
 
 			# Calculate loss
 			loss = max(0, np.dot(b, x) - self.getEpsilon())
-			if isnan(loss):
-				print('\n\n\n\t\tloss is nan at (' + str(i) + ')\n\n\n')
-				
 
 			# Calculate Tau (update step size)
 			tau = loss / ((norm(np.subtract(x, np.mean(x))) ** 2) + (1 / (2. * self.getSlack()))) 
 
-			if isnan(tau):
-				print('\n\n\n\t\ttau is nan at (' + str(i) + ')\n\n\n')
-
 			# Calculate new portfolio weights
 			b = np.subtract(self.getWeights(), np.multiply(tau, np.subtract(x, np.mean(x))))
-			for j in range(len(b)):
-				if isnan(b[j]):
-					print('\n\n\n\t\tb is nan at (' + str(i) + ', ' + str(j) + ')\n\n\n')
 			
-		#	print('b: ' + str(b))			
-
 			# Project portfolio into simplex domain
 			result = minimize(lambda q: norm(np.subtract(q, b)) ** 2, [1. / len(b) for z in b], method='SLSQP', bounds=[(0.0, 1.0) for z in b], constraints={'type': 'eq', 'fun': lambda q: sum(q) - 1.0})
 			
 			if not self.noop:
-			#	print('Result: ' + str(result['x']))
-				for j in range(len(result['x'])):
-					if isnan(result['x'][j]):
-						print('\n\n\n\t\tresult[\'x\'] is nan at (' + str(i) + ', ' + str(j) + ')\n\n\n')
 				self.updatePortfolio(result['x'], prevWeights, prevValue, prevRates, curRates) 
-		print('\nFinal weights: ' + str(self.getWeights()) + '\n')
 		return self.getValue()
 
 	def getEpsilon(self):
@@ -191,6 +144,7 @@ def getData(b, t, depth, symbol):
 			break
 	return data
 
+# Make sure there are no gaps or repeats in a sequence of timesteps
 def validateTimesteps(data):
 	print('Final data len: ' + str(len(data)))
 	timesteps = [x[0] for x in data]
@@ -201,12 +155,14 @@ def validateTimesteps(data):
 			return False
 	return True
 
+# Trim the starts of sequences to ensure similar length for all symbols
 def truncateData(data):
 	truncLength = min([len(sym) for sym in data])
 	print('Truncated data len: ' + str(truncLength))
 	#return [x[:truncLength] for x in data]
 	return [x[len(x) - truncLength:] for x in data]
 
+# Ensures the start of each sequence is the same after being truncated
 def checkTruncData(data):
 	timestamp = data[0][0][0]
 	for i in range(1, len(data)):
@@ -215,6 +171,7 @@ def checkTruncData(data):
 			return
 	print('\t\t\tTimestamps synchronized')
 
+# Reformat the data into a single multivariate sequence
 def formatData(data):
 	# (symbols x timesteps x features) --> (timesteps x symbols)
 	fData = []
@@ -231,7 +188,6 @@ start = now - 500 * 60000
 binance = ccxt.binance()
 binance.load_markets()
 symbols = ['ETH/BTC', 'XRP/BTC', 'XLM/BTC', 'ADA/BTC', 'NEO/BTC', 'XMR/BTC', 'XEM/BTC', 'EOS/BTC', 'ICX/BTC', 'LTC/BTC', 'QTUM/BTC', 'VEN/BTC', 'NAV/BTC', 'BQX/BTC', 'NEO/BTC']
-#depth = 140000
 depth = 110000
 
 print('\nPortfolio symbols: ' + str(symbols))
@@ -260,15 +216,12 @@ port0 = Portfolio(symbols, 0.65, 5, b)
 port1 = Portfolio(symbols, 0.75, 5, b)
 port2 = Portfolio(symbols, 0.85, 5, b)
 port3 = Portfolio(symbols, 0.95, 5, b)
-port4 = Portfolio(symbols, 0.65, 4, b)
-port5 = Portfolio(symbols, 0.75, 4, b)
-port6 = Portfolio(symbols, 0.85, 4, b)
-port7 = Portfolio(symbols, 0.95, 4, b)
-port8 = Portfolio(symbols, 0.65, 3, b)
-port9 = Portfolio(symbols, 0.75, 3, b)
-port10 = Portfolio(symbols, 0.85, 3, b)
-port11 = Portfolio(symbols, 0.95, 3, b)
-ports = [port0, port1, port2, port3, port4, port5, port6, port7, port8, port9, port10, port11]
+port4 = Portfolio(symbols, 0.45, 6, b)
+port5 = Portfolio(symbols, 0.55, 6, b)
+port6 = Portfolio(symbols, 0.65, 6, b)
+port7 = Portfolio(symbols, 0.75, 6, b)
+
+ports = [port0, port1, port2, port3, port4, port5, port6, port7]
 bh = Portfolio(symbols, 0.95, 3, b, noop=True)
 
 print('\nBuy & Hold:')
